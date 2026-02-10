3f06a3abfea12ddc3e09db6061c6ed643a0943ae
Payal Sahu <payalsahu@Payals-MacBook-Air.local>
2026-02-11 03:10:54 +0530

    feb 11 complete

--- DP/DPOnStrings/LongestPalindromicSubsequence.java
+++ DP/DPOnStrings/LongestPalindromicSubsequence.java
@@ -1,5 +1,19 @@
 package A2Z.DP.DPOnStrings;
 
 public class LongestPalindromicSubsequence {
-    private static int longestCommonSubsequence(String text1, String text2) {
-    }}
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder rev = new StringBuilder(s);
+        String r = rev.reverse().toString();
+        int n=s.length();
+        int[][] dp=new int[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==r.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }return dp[n][n];
+    }
+}
